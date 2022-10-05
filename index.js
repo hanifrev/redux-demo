6d29612ce217e0bd07749e128aed276d747b31f9
hanifrev <hanifrev@gmail.com>
2022-10-05 09:20:11 +0700

    separate initial state & reducer

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const redux = require("redux");
 const createStore = redux.createStore;
-
-const CAKE_ORDERED = "CAKE_ORDERED";
-const CAKE_RESTOCKED = "CAKE_RESTOCKED";
-const JUICE_RESTOCKED = "JUICE_RESTOCKED";
+const bindActionCreators = redux.bindActionCreators;
+const combineReducers = redux.combineReducers;
 
 // action
+const CAKE_ORDERED = "CAKE_ORDERED";
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
+const JUICE_ORDERED = "JUICE_ORDERED";
+const JUICE_RESTOCKED = "JUICE_RESTOCKED";
+
+// action creator
 function orderCake() {
   return {
     type: CAKE_ORDERED,
@@ -20,6 +24,13 @@ function restockCake(qty = 1) {
   };
 }
 
+function orderJuice() {
+  return {
+    type: JUICE_ORDERED,
+    payload: 1,
+  };
+}
+
 function juiceRestock(qty = 1) {
   return {
     type: JUICE_RESTOCKED,
@@ -27,40 +38,64 @@ function juiceRestock(qty = 1) {
   };
 }
 
-const initialState = {
+// const initialState = {
+//   numOfCakes: 10,
+//   anotherProperty: 0,
+//   juice: 13,
+// };
+
+const initialCakeState = {
   numOfCakes: 10,
-  anotherProperty: 0,
+};
+
+const initialJuiceState = {
   juice: 13,
 };
 
 //  (previousState, action) => newState
 
-const reducer = (state = initialState, action) => {
+const cakeReducer = (state = initialCakeState, action) => {
   switch (action.type) {
     case CAKE_ORDERED:
       return {
         ...state,
         numOfCakes: state.numOfCakes - 1,
-        juice: state.juice - 1,
       };
     case CAKE_RESTOCKED:
       return {
         ...state,
         numOfCakes: state.numOfCakes + action.payload,
       };
-    case JUICE_RESTOCKED: {
-      return {
-        ...state,
-        juice: state.juice + action.payload,
-      };
-    }
     default:
       return state;
   }
 };
 
+const juiceReducer = (state = initialJuiceState, action) => {
+  switch (action.type) {
+    case JUICE_ORDERED:
+      return {
+        ...state,
+        juice: state.juice - 1,
+      };
+    case JUICE_RESTOCKED:
+      return {
+        ...state,
+        juice: state.juice + action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+// for multiple reducers
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  thejuice: juiceReducer,
+});
+
 // hold app state
-const store = createStore(reducer);
+const store = createStore(rootReducer);
 
 // allow access to state via getState()
 console.log("initial state", store.getState());
@@ -72,11 +107,20 @@ const unsubscribe = store.subscribe(() =>
 );
 
 // allow state to be updated via dispatch(action)
-store.dispatch(orderCake());
-store.dispatch(orderCake());
-store.dispatch(orderCake());
-store.dispatch(restockCake(7));
-store.dispatch(juiceRestock(2));
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(restockCake(7));
+// store.dispatch(juiceRestock(2));
 
+const actions = bindActionCreators(
+  { orderCake, restockCake, orderJuice, juiceRestock },
+  store.dispatch
+);
+actions.orderCake();
+actions.orderCake();
+actions.restockCake(3);
+actions.orderJuice();
+actions.juiceRestock(8);
 // Handles unregistering of listeners via the function returned by subscribe(listener) / unsubscribe
 unsubscribe();
